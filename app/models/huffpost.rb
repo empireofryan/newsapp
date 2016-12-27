8530ee10730fa218a77e52f30174ea6d14c62546
empireofryan <rjohnson2001@gmail.com>
2016-12-27 18:46:34 -0500

    latest designs

--- app/models/huffpost.rb
+++ app/models/huffpost.rb
@@ -1,3 +1,11 @@
 class Huffpost < ApplicationRecord
   scope :past_day, -> { where("created_at > ?", Time.now-1.days) }
+
+  def odd?(n)
+    if n % 2 == 0
+      @num = false
+    end
+    @num
+  end
+  
 end
